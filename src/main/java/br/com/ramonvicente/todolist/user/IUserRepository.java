dfd280183ec4e9ad093b8aff20cd750226cd0a51
Ramon Vicente <ramon.silva.1396106@sga.pucminas.br>
2023-10-10 19:52:13 -0300

    Integração com DB local

---
+++ src/main/java/br/com/ramonvicente/todolist/user/IUserRepository.java
@@ -0,0 +1,8 @@
+package br.com.ramonvicente.todolist.user;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+    UserModel findByUserName(String userName);
+}
--- src/main/java/br/com/ramonvicente/todolist/user/UserController.java
+++ src/main/java/br/com/ramonvicente/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.ramonvicente.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class UserController {
     
+    @Autowired
+    private IUserRepository userRepository;
 
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.name);
+    public UserModel create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUserName(userModel.getUserName());
+
+        if (user != null) {
+            throw new RuntimeException("User already exists");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
     }
 }
